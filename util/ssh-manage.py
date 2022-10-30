f292fd67e07f641a9fc2645d89cd25300133db1b
Akil Marshall <akilmarshall@gmail.com>
2022-10-30 03:30:51 -1000

    setup the ability to discern which subcommand was called, also wrote the match structure to deal with each subcommand

--- util/ssh-manage.py
+++ util/ssh-manage.py
@@ -17,13 +17,27 @@ def main():
 
     parser = ArgumentParser(description=__doc__)
     parser.add_argument('--config', default=config_path, help=f'Path to configuration, specified as a path. default:{config_path}')
-    subparsers = parser.add_subparsers(help='sub-command help')
+    subparsers = parser.add_subparsers(dest='sub', help='sub-command help')
     list_parser = subparsers.add_parser('list', help='list all services (active and inactive)')
     create_parser = subparsers.add_parser('create',  help='create service (make a systemd unit and add to service file')
     remove_parser = subparsers.add_parser('remove', help='remove service, delete from configuration, and delete from service file')
     toggle_parser = subparsers.add_parser('toggle', help='toggle service (set enabled, set disabled, toggle)')
     config_parser = subparsers.add_parser('configure', help='configure (update the services configuration)')
+
     args = parser.parse_args()
+    match args.sub:
+        case 'list':
+            pass
+        case 'create':
+            pass
+        case 'remove':
+            pass
+        case 'toggle':
+            pass
+        case 'configure':
+            pass
+        case None:
+            pass
 
 if __name__ == '__main__':
     main()
