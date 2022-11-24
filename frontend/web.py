899fc586a4d9b1c86705599f593dc822ffa12277
Akil Marshall <akilmarshall@gmail.com>
2022-11-23 23:54:09 -1000

    factored out a call to Path.absolute

--- frontend/web.py
+++ frontend/web.py
@@ -1,11 +1,14 @@
 import hug
 from jinja2 import Template
+from pathlib import Path
 
+
+pages_path = Path('pages').absolute()
 
 @hug.get('/', output=hug.output_format.html)
 def home():
     # Get File Content in String
-    jinja2_template_string = open('pages/home.html', 'r').read()
+    jinja2_template_string = open(pages_path / 'home.html', 'r').read()
 
     # Create Template Object
     template = Template(jinja2_template_string)
@@ -15,10 +18,9 @@ def home():
 
     return html_template_string
 
-
 @hug.get('/services', output=hug.output_format.html)
 def services():
-    jinja2_template_string = open('pages/services.html', 'r').read()
+    jinja2_template_string = open(pages_path / 'services.html', 'r').read()
     template = Template(jinja2_template_string)
     html_template_string = template.render()
 
